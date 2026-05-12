6735f96d472b765621cec4ad984f984a55a2e24f
Burak <burakakbilek07@gmail.com>
2026-05-12 21:12:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let activeSection = null;
 
+function showSection(id) {
+    const section = document.getElementById(id);
+
+    if (activeSection === section) {
+        section.style.display = "none";
+        activeSection = null;
+        return;
+    }
+
+    const sections = document.querySelectorAll(".section");
+    sections.forEach(sec => sec.style.display = "none");
+
+    section.style.display = "block";
+    activeSection = section;
+}
