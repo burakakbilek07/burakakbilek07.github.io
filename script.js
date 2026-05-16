ba029c4fce26773fbbb0c15b4fae7a1f9e711bae
Burak <burakakbilek07@gmail.com>
2026-05-17 01:07:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-let activeSection = null;
-
-function showSection(id) {
-    const section = document.getElementById(id);
-
-    if (activeSection === section) {
-        section.style.display = "none";
-        activeSection = null;
-        return;
+function scrollToSection(sectionId) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
     }
-
-    const sections = document.querySelectorAll(".section");
-    sections.forEach(sec => sec.style.display = "none");
-
-    section.style.display = "block";
-    activeSection = section;
 }
