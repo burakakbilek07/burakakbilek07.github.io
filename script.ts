54b85b0b2bf9fa73f0ac43a5355eb9aef39b6556
Burak <burakakbilek07@gmail.com>
2026-05-17 20:30:53 +0300

    Update and rename script.ts to script.js

--- script.ts
+++
@@ -1,6 +0,0 @@
-function scrollToSection(sectionId: string): void {
-    const element: HTMLElement | null = document.getElementById(sectionId);
-    if (element) {
-        element.scrollIntoView({ behavior: 'smooth' });
-    }
-}
